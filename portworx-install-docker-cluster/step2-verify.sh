4791a61c5c5d49697e5f84ba6b279df9c782d3e1
Gabriela S. Soria <soria.gaby@gmail.com>
2019-11-15 15:54:53 -0800

    Changed verification script for step 2
    Signed-off-by: Gabriela S. Soria <soria.gaby@gmail.com>

--- portworx-install-docker-cluster/step2-verify.sh
+++ portworx-install-docker-cluster/step2-verify.sh
@@ -1,5 +1,5 @@
-docker container inspect etcd | grep -i hostport| grep  -q 4001
-if [ $? -eq 0 ]
-   then	
-echo "done"
+if [[ $(docker container inspect etcd | grep -i hostport| grep  -q 4001) ]]; then
+    echo "done"
+else
+    echo "no containers found"
 fi
