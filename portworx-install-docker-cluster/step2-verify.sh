e256af0427577e46b3b0078ab5f0466a1b4ea328
Gabriela S. Soria <soria.gaby@gmail.com>
2019-11-16 01:01:21 -0800

    Test of no passing validation on step 2
    Signed-off-by: Gabriela S. Soria <soria.gaby@gmail.com>

--- portworx-install-docker-cluster/step2-verify.sh
+++ portworx-install-docker-cluster/step2-verify.sh
@@ -1,5 +1,6 @@
-HOSTPORTOUTPUT=$(echo $(docker inspect $(docker ps | grep etcd | awk '{print $1}') | grep -i hostport| grep -oP '(?<="HostPort": ")(.*)(?=")'| sort --uniq))
-ECHOPORT=$(echo 4001)
-if [ "$HOSTPORTOUTPUT" == "$ECHOPORT" ]; then
-    echo "done"
-fi
+# HOSTPORTOUTPUT=$(echo $(docker inspect $(docker ps | grep etcd | awk '{print $1}') | grep -i hostport| grep -oP '(?<="HostPort": ")(.*)(?=")'| sort --uniq))
+# ECHOPORT=$(echo 4001)
+# if [ "$HOSTPORTOUTPUT" == "$ECHOPORT" ]; then
+#     echo "done"
+# fi
+echo "nodone"
